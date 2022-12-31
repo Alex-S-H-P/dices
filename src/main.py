ce8eae9672ce094ccb483324857c01077cdc550e
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2023-01-01 00:12:48 +0100

    Adding more command versatility !

--- src/main.py
+++ src/main.py
@@ -1,21 +1,40 @@
 import re
+import typing
+
 import discord
 import dices
 import errors
 import sys
 
+call = typing.Callable[[str, ...], None]
 
-things_to_say : list[str] = []
+things_to_say : list[tuple[str, str]] = []
 
 
 def say(thing:str)-> None:
-    things_to_say.append(thing)
+    things_to_say.append((thing, ""))
+
+def to(chan : str) -> None:
+    if len(things_to_say)> 0:
+        things_to_say[-1][1] = chan
 
 client = discord.Client()
 d_probas: dict[int, dict] = {}
 d_value: dict[int, float] = {}
-say_command : list[str] = ["say", "--say", "-s", "tell", "write", "print"]
 
+
+say_command : list[str] = ["say", "--say", "-s", "tell", "write", "print"]
+to_command : list[str] = ["to", "--to", "-t", ]
+specific_command_types : list[typing.Union[list[tuple[call, int] , str]]] = [
+    [(say, 1)] + say_command,
+    [(to, 1)] + to_command
+]
+
+commands : dict[str, tuple[call, int]] = {}
+for command_type in specific_command_types:
+    cmd = command_type[0]
+    for command in command_type[1:]:
+        commands[command] = cmd
 
 def clean(txt: str) -> str:
     return re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]').sub("", txt)
@@ -26,15 +45,16 @@ async def on_ready():
     print(f"Logged in successfully as {client.user}",
           f"\n\t{things_to_say}" if len(things_to_say) > 0 else
           "", sep="")
-    for server in client.guilds:
-        for channel in server.text_channels:
-            print(f"Looking at the channel {channel.name} of the server {server}")
-            if "bots" in channel.name:
-                print(f"\tfound channel to print in : {channel.name}")
-                for thing in things_to_say:
-                    print("\tsaying", thing)
-                    await client.get_channel(channel.id).send(thing)
-                break
+
+    for thing in things_to_say:
+        print(f"Trying to say {thing[0]}",
+              f" to {thing[1]}" if thing[1] else ".", sep="")
+        for server in client.guilds:
+            for channel in server.text_channels:
+                print(f"\tLooking at channel {channel.name} of server {server.name}")
+                if ((not thing[1] or server.name == thing[1]) and "test-bots" in channel.name) or channel.name == thing[1]:
+                    print("\t\tMatched !")
+                    await client.get_channel(channel.id).send(thing[0])
 
 
 @client.event
@@ -83,7 +103,14 @@ async def on_message(message):
 if __name__ == '__main__':
     pointer : int  = 1
     print(sys.argv, sys.argv[pointer], "found" if sys.argv[pointer] in say_command else "not found")
-    while len(sys.argv) > pointer + 1 and sys.argv[pointer] in say_command:
-        things_to_say.append(sys.argv[pointer + 1])
-        pointer += 2
+    while len(sys.argv) > pointer:
+        cur_arg = sys.argv[pointer]
+        cmd = commands[cur_arg]
+        if cur_arg in commands and pointer + cmd[1] < len(sys.argv):
+            print(cmd)
+            print(sys.argv[pointer+1:], sys.argv[:pointer+cmd[1]])
+            cmd[0](*(sys.argv[pointer+1:pointer+cmd[1]+1] if cmd[1] > 0 else [])) # we load the sys.argv into the right arguments
+
+            pointer += cmd[1]
+        pointer += 1
     client.run(open("./key.key", "r").read())
